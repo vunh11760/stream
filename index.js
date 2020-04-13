0b4378f10170ca8130aa66a72defbae8b7134b83
nguyen vu <nguyenhoangvu11760@gmail.com>
2020-04-13 16:03:44 +0700

    update

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
     res.render('Home')
 })
 
-app.listen(3000, () => console.log('Server started')); 
+app.listen(process.env.PORT || 3000, () => console.log('Server started')); 
